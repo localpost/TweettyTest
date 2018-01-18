01b342954f6b147beabdb853d90422c4579cbef3
localpost <localpost@2ch.hk>
2018-01-18 12:09:07 +0500

    Add project

---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.Text;
+using TweetSharp;
+using System.Net;
+
+namespace TwittyTest
+{
+    class Program
+    {
+        //Получаем зашифрованные ключи от API твиттера из файла конфигурации
+        static string Key = ConfigurationSettings.AppSettings.GetValues("ConsumerKey")[0];
+        static string Secret = ConfigurationSettings.AppSettings.GetValues("ConsumerSecret")[0];
+        static string TokenKey = ConfigurationSettings.AppSettings.GetValues("TokenKey")[0];
+        static string TokenSecret = ConfigurationSettings.AppSettings.GetValues("TokenSecret")[0];      
+
+        static void Main(string[] args)
+        {
+            TwitService Service = new TwitService(Encrypt(Key, 2), Encrypt(Secret, 2), Encrypt(TokenKey, 2), Encrypt(TokenSecret, 2));
+            bool exit = false;
+            string userName = "";
+            char[] abc = MakeABC('a', 'z');
+
+            while (!exit)
+            {
+                
+                Console.Write("Введите имя (для выхода введите 'Q'): ");
+                userName = Console.ReadLine();
+                if (userName.ToLower() != "q")
+                {
+                    try
+                    {
+                        List<string> tweets = Service.GetTweets(userName, 5);
+                        Dictionary<char, double> stats = Service.GetStatystics(abc, tweets);
+                        string json = Service.DictionaryToJSON(stats);
+                        Service.SendTweet(userName, json);
+                        PrintStats(stats);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                else
+                {
+                    exit = true;
+                }
+            }
+        }
+
+        //Простенький Шифратор/Дешифратор
+        static string Encrypt(string _plaintext, int _key)
+        {
+            string result = "";
+            for (int i = 0; i < _plaintext.Length; i++)
+            {
+                result += (char)((int)_plaintext[i] ^ _key);
+            }
+            return result;
+        }
+
+        //Генератор массива символов
+        static char[] MakeABC(char _start, char _end)
+        {
+            _start = Char.ToUpper(_start);
+            _end = Char.ToUpper(_end);
+
+            string temp = "";
+            if (_start < _end)
+            {
+                for (char letter = _start; letter <= _end; letter++)
+                {
+                    temp += letter;
+                }
+            }
+            else
+            {
+                for (char letter = _start; letter >= _end; letter--)
+                {
+                    temp += letter;
+                }
+            }
+            return temp.ToCharArray();
+        }
+
+        //Вывод JSON объекта на консоль
+        static void PrintJSON(string _data)
+        {
+            Console.WriteLine("Статистика по данному пользователю:");
+            foreach (char _c in _data.Skip(1))
+            {
+                if (_c != ',' && _c != '}')
+                {
+                    Console.Write(_c);
+                }
+                else
+                {
+                    Console.Write('\n');
+                }
+            }
+        }
+
+        //Вывод статистики на консоль
+        static void PrintStats(Dictionary<char, double> stats)
+        {
+            Console.WriteLine("Статистика по данному пользователю:");
+            foreach (var item in stats)
+            {
+                Console.WriteLine(item.Key.ToString() + ":'" + item.Value.ToString().Replace(",", ".") + "'\n");
+            }
+        }
+    }
+}
---
+++ TwitService.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TweetSharp;
+using System.Net;
+
+namespace TwittyTest
+{
+    class TwitService
+    {
+        private TwitterService tService;
+
+        public TwitService(string ConsumerKey, string ConsumerSecret, string TokenKey, string TokenSecret)
+        {
+            tService = new TwitterService(ConsumerKey, ConsumerSecret, TokenKey, TokenSecret);
+            //getTweetsHandler = new GetStats(GetTweets);
+        }        
+
+        //Отправить статус в Твиттер
+        public bool SendTweet(string _sendTo, string _message)
+        {
+            bool sendStatus = false;
+
+            if (_sendTo[0] != '@')
+            {
+                _sendTo = "@" + _sendTo.Replace(" ", "");
+            }
+
+            tService.SendTweet(new SendTweetOptions { Status = _sendTo + ", статистика для последних 5 твитов:" + _message }, (tweet, response) =>
+            {
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    sendStatus = true;
+                }
+            });
+
+            return sendStatus;
+        }
+
+        //Получить последние N-число статусов твиттера _userName
+        public List<string> GetTweets(string _userName, int _count)
+        {            
+            if (_count > 0)
+            {
+                try
+                {
+                    List<string> result = new List<string>();
+                    //Поиск Id пользователя по имени _userName
+                    long userId = tService.GetUserProfileFor(new GetUserProfileForOptions { ScreenName = _userName }).Id;
+
+                    //Поиск N-число твиттов пользователя c UserID
+                    IEnumerable<TwitterStatus> ListTweets = tService.ListTweetsOnUserTimeline(new ListTweetsOnUserTimelineOptions
+                    {
+                        UserId = userId,
+                        Count = _count
+                    });
+
+                    foreach (var item in ListTweets)
+                    {
+                        result.Add(item.Text);
+                    }
+                    return result;
+                }
+                catch (Exception e)
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+            
+        }
+
+        //Получить статистку по частотности символов в строке
+        public Dictionary<char, double> GetStatystics(char[] _dictionary, List<string> _listOfTweets)
+        {
+            Dictionary<char, double> result = new Dictionary<char, double>();
+
+            for(int i=0; i<_dictionary.Length;i++)
+            {
+                char item = Char.ToLower(_dictionary[i]);
+                double count = 0;
+                double allTweetsLetters = 0;
+                double frequency = 0;
+
+                foreach (string tweet in _listOfTweets)
+                {
+                    count += tweet.ToLower().Count(letter => letter == item);
+                    allTweetsLetters += tweet.Count(char.IsLetter);
+                }
+
+                frequency = count / allTweetsLetters;
+
+                result.Add(item, Math.Round(frequency,2));
+            }
+
+            return result;
+        }
+
+        //Преобразование Dictionary в JSON
+        public string DictionaryToJSON(Dictionary<char, double> _dictionary)
+        {
+            string result = "{";
+            foreach(var item in _dictionary)
+            {
+                result += item.Key.ToString() + ":'" + item.Value.ToString().Replace(",", ".") + "',";
+            }
+            if(result.Length>1)
+            {
+                result = result.Substring(0, result.Length - 1);
+            }
+            result +="}";
+            return result;
+        }
+    }
+}
